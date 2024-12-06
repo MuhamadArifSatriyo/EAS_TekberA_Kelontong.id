bfe6f1029c5c9d55d09ae7b915ce6d2b2fdddee1
alhfh <namedauliah@gmail.com>
2024-12-06 08:55:57 +0700

    test

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:inventory_manager/widget/kelontong_drawer.dart';
 import '../screen/tambah_barang.dart';
 import '../screen/detail_barang.dart';
-import '../screen/profile.dart'; // Import untuk layar ProfileScreen
+import '../screen/profile.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
@@ -19,7 +19,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   List<Map<String, dynamic>> _inventory = [];
   String _searchQuery = '';
-  String _namaToko = 'Toko Anda'; // Variable untuk menyimpan nama toko
+  String _namaToko = 'Toko Anda'; // Variable to store store name
   final TextEditingController _searchController = TextEditingController();
 
   final List<String> _categories = [
@@ -38,13 +38,7 @@ class _HomeScreenState extends State<HomeScreen> {
     _loadNamaToko();
   }
 
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    _loadNamaToko(); // Perbarui nama toko setiap kali widget kembali aktif
-  }
-
-  // Fungsi untuk memuat nama toko dari SharedPreferences
+  // Load store name from SharedPreferences
   Future<void> _loadNamaToko() async {
     final prefs = await SharedPreferences.getInstance();
     setState(() {
@@ -52,7 +46,7 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  // Fungsi untuk memuat data inventaris dari file lokal
+  // Load inventory from text file
   Future<void> _loadInventory() async {
     final directory = await getApplicationDocumentsDirectory();
     final file = File('${directory.path}/inventory.txt');
@@ -66,7 +60,7 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-  // Fungsi untuk menyimpan data inventaris ke file lokal
+  // Save inventory to text file
   Future<void> _saveInventory() async {
     final directory = await getApplicationDocumentsDirectory();
     final file = File('${directory.path}/inventory.txt');
@@ -83,13 +77,29 @@ class _HomeScreenState extends State<HomeScreen> {
         _inventory[index] = itemData; // Update item
       }
     });
-    _saveInventory();
+    _saveInventory(); // Simpan data yang telah diperbarui
   }
 
   void _onDeleteItem(Map<String, dynamic> item) {
     setState(() {
       _inventory.removeWhere(
-          (existingItem) => existingItem['name'] == item['name']); // Hapus item
+        (existingItem) => existingItem['name'] == item['name']
+      ); // Hapus item
+    });
+    _saveInventory(); // Simpan data setelah dihapus
+  }
+
+void _handleAddItem(Map<String, dynamic> itemData) {
+    setState(() {
+      _inventory.add({
+        'name': itemData['nama'],
+        'category': itemData['kategori'],
+        'stock': itemData['stok'],
+        'price': itemData['harga'],
+        'description': itemData['deskripsi'],
+        'status': _getStockStatus(itemData['stok']),
+        'imagePath': itemData['imagePath'],
+      });
     });
     _saveInventory();
   }
@@ -113,6 +123,12 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  // Fungsi untuk debugging SharedPreferences
+  Future<void> printSharedPreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+    debugPrint('Data SharedPreferences: ${prefs.getString('namaToko')}');
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -122,25 +138,28 @@ class _HomeScreenState extends State<HomeScreen> {
           backgroundColor: const Color.fromARGB(255, 255, 255, 255),
           elevation: 0,
           leading: Builder(
-            builder: (context) => IconButton(
-              icon: const Icon(Icons.sort_rounded, color: Colors.black),
-              onPressed: () {
-                Scaffold.of(context).openDrawer(); // Tetap membuka drawer
-              },
-            ),
+            builder: (context) {
+              return IconButton(
+                icon: const Icon(Icons.sort_rounded, color: Colors.black),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+              );
+            },
           ),
           title: Text.rich(
             TextSpan(
-              text: 'Halo, ',
+              text: 'Halo, ', 
               style: const TextStyle(
-                fontWeight: FontWeight.bold,
+                fontWeight: FontWeight.bold, 
               ),
               children: [
                 TextSpan(
-                  text: _namaToko,
+                  text: '$_namaToko', 
                   style: const TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.normal,
+                    color: Colors.black, 
+                    fontWeight:
+                        FontWeight.normal,
                   ),
                 ),
               ],
@@ -148,6 +167,10 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           centerTitle: true,
           bottom: TabBar(
+            onTap: (index) {
+              setState(() {
+              });
+            },
             isScrollable: true,
             labelColor: Colors.blue,
             unselectedLabelColor: Colors.black,
@@ -239,8 +262,7 @@ class _HomeScreenState extends State<HomeScreen> {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) =>
-                    TambahBarang(onAddItem: _handleAddItem),
+                builder: (context) => TambahBarang(onAddItem: _handleAddItem),
               ),
             );
           },
@@ -297,60 +319,64 @@ class _HomeScreenState extends State<HomeScreen> {
                         width: double.infinity,
                         fit: BoxFit.cover,
                       ),
+              )
+            else
+              Container(
+                height: 100,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: const Icon(
+                  Icons.image_not_supported,
+                  color: Colors.grey,
+                  size: 48,
+                ),
               ),
-            const SizedBox(height: 8.0),
+            const SizedBox(height: 8),
             Text(
               item['name'],
               style: const TextStyle(
-                fontSize: 14.0,
-                fontWeight: FontWeight.bold,
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
               ),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
             ),
-            const SizedBox(height: 4.0),
+            const SizedBox(height: 8),
             Text(
               item['category'],
               style: TextStyle(
-                fontSize: 12.0,
+                fontSize: 12,
                 color: Colors.grey[600],
               ),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
             ),
-            const SizedBox(height: 4.0),
+            const SizedBox(height: 8),
             Text(
-              'Stok: ${item['stock']}',
-              style: TextStyle(
-                fontSize: 12.0,
-                color: Colors.grey[600],
-              ),
-            ),
-            const SizedBox(height: 8.0),
-            Text(
-              'Rp${item['price'].toString()}',
+              'Rp ${item['price'].toString()}',
               style: const TextStyle(
-                fontSize: 14.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.blue,
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
               ),
             ),
-            const SizedBox(height: 8.0),
-            Row(
-              children: [
-                Container(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 8.0, vertical: 4.0),
-                  decoration: BoxDecoration(
-                    color: _getStatusColor(item['status']).withOpacity(0.1),
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  child: Text(
-                    item['status'],
-                    style: TextStyle(
-                      fontSize: 10.0,
-                      fontWeight: FontWeight.bold,
-                      color: _getStatusColor(item['status']),
-                    ),
-                  ),
+            const SizedBox(height: 8),
+            Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+              decoration: BoxDecoration(
+                color: _getStatusColor(item['status']),
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              child: Text(
+                item['status'],
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
                 ),
-              ],
+              ),
             ),
           ],
         ),
