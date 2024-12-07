3db8acc0ec9a7ed9b028ac4f9182df0ace315b5b
Aulia Hanifah <138230136+auliahanifh@users.noreply.github.com>
2024-12-07 15:55:58 +0700

    upload main.dart

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import '../screen/welcome_screen.dart';
+import '../screen/home_screen.dart';
+import '../screen/dashboard_screen.dart';
+import '../screen/about_us_screen.dart';
+import '../screen/profile.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Check if profile.txt exists
+  final directory = await getApplicationDocumentsDirectory();
+  final profileFile = File('${directory.path}/profile.txt');
+
+  runApp(MyApp(initialRoute: await profileFile.exists() ? '/home' : '/'));
+}
+
+class MyApp extends StatelessWidget {
+  final String initialRoute;
+  MyApp({required this.initialRoute});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(scaffoldBackgroundColor: Colors.white),
+      initialRoute: initialRoute, // Define the initial route
+      routes: {
+        '/': (context) => WelcomeScreen(), 
+        '/home': (context) => HomeScreen(), 
+        '/dashboard': (context) => DashboardScreen(), 
+        '/aboutUs': (context) => AboutUsScreen(), 
+        '/profile': (context) => ProfileScreen(), 
+      },
+    );
+  }
+}
