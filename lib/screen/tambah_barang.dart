0f9bed5629bc40a65e69796dcb4d609a338a03ff
Aulia Hanifah <138230136+auliahanifh@users.noreply.github.com>
2024-12-07 15:58:51 +0700

    upload files

---
+++ lib/screen/tambah_barang.dart
@@ -0,0 +1,374 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'dart:convert';
+
+import 'package:inventory_manager/screen/home_screen.dart';
+
+class TambahBarang extends StatefulWidget {
+  final Function(Map<String, dynamic>) onAddItem;
+  final Map<String, dynamic>? item;
+
+  const TambahBarang({Key? key, required this.onAddItem, this.item}) : super(key: key);
+
+  @override
+  _TambahBarangState createState() => _TambahBarangState();
+}
+
+
+
+class _TambahBarangState extends State<TambahBarang> {
+  final _formKey = GlobalKey<FormState>();
+  final _namaBarangController = TextEditingController();
+  final _stokController = TextEditingController();
+  final _hargaController = TextEditingController();
+  final _deskripsiController = TextEditingController();
+  String _selectedCategory = 'Makanan';
+  int _stock = 0;
+  
+  File? _imageFile;
+  String? _webImageUrl;
+  final ImagePicker _picker = ImagePicker();
+
+  final List<String> _categories = [
+    'Makanan',
+    'Sayuran',
+    'Peralatan Rumah Tangga',
+    'Minuman',
+    'Lainnya',
+  ];
+
+  String _getStockStatus(int stock) {
+    if (stock > 10) return 'Stok Aman';
+    if (stock > 0) return 'Stok Menipis';
+    return 'Stok Habis';
+  }
+
+  Future<void> _pickImage(ImageSource source) async {
+    try {
+      final XFile? pickedFile = await _picker.pickImage(
+        source: source,
+        maxWidth: 800,
+        maxHeight: 800,
+        imageQuality: 85,
+      );
+
+      if (pickedFile != null) {
+        if (kIsWeb) {
+          final bytes = await pickedFile.readAsBytes();
+          setState(() {
+            _webImageUrl = 'data:image/jpeg;base64,${base64Encode(bytes)}';
+          });
+        } else {
+          setState(() {
+            _imageFile = File(pickedFile.path);
+          });
+        }
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Gagal Mengunggah Gambar: $e')),
+      );
+    }
+  }
+
+  void _incrementStock() {
+    setState(() {
+      _stock++;
+      _stokController.text = _stock.toString(); // Update the controller text to reflect stock change
+    });
+  }
+
+  void _decrementStock() {
+    setState(() {
+      if (_stock > 0) {
+        _stock--;
+        _stokController.text = _stock.toString(); // Update the controller text to reflect stock change
+      }
+    });
+  }
+
+   @override
+  void initState() {
+    super.initState();
+    print(widget.item.toString());
+    print("edit data");
+    // Jika item tersedia, isi form dengan nilai awal
+    if (widget.item != null) {
+      _stock = widget.item!['stock'] ?? 0;
+      _namaBarangController.text = widget.item!['name'] ?? '';
+      _selectedCategory = widget.item!['category'] ?? 'Makanan';
+      _stokController.text = widget.item!['stock'].toString() ?? "";
+      _hargaController.text = widget.item!['price']?.toString() ?? '';
+      _deskripsiController.text = widget.item!['description'] ?? '';
+      if (kIsWeb) {
+        _webImageUrl = widget.item!['imagePath'];
+      } else {
+        final imagePath = widget.item!['imagePath'];
+        if (imagePath != null && imagePath.isNotEmpty) {
+          _imageFile = File(imagePath);
+        }
+      }
+    }
+
+    setState(() {
+      
+    });
+  }
+
+  void _saveItem() {
+    if (_formKey.currentState!.validate()) {
+  
+    print(_namaBarangController.text);
+      if(widget.item != null){
+        print("TIDAK KOSONG");
+          var itemData = {
+        'name': _namaBarangController.text,
+        'category': _selectedCategory,
+        'stock': int.parse(_stokController.text),
+        'status': _getStockStatus(int.parse(_stokController.text)),
+        'price': double.tryParse(_hargaController.text.replaceAll(RegExp('[^0-9.]'), '')) ?? 0.0,
+        'description': _deskripsiController.text,
+        'imagePath': kIsWeb ? _webImageUrl : _imageFile?.path};
+        print(itemData.toString());
+        print("go to");
+         widget.onAddItem(itemData);
+
+      }else{
+
+    final itemData = {
+        'nama': _namaBarangController.text,
+        'kategori': _selectedCategory,
+        'stok': _stock,
+        'harga': double.tryParse(_hargaController.text.replaceAll(RegExp('[^0-9.]'), '')) ?? 0.0,
+        'deskripsi': _deskripsiController.text,
+        'imagePath': kIsWeb ? _webImageUrl : _imageFile?.path,
+      };
+      widget.onAddItem(itemData);
+
+
+      }
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomeScreen(),));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.item == null ? 'Tambah Barang' : 'Edit Barang'),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        foregroundColor: Colors.black,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: () => _pickImage(ImageSource.gallery),
+                  child: Container(
+                    width: double.infinity,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(color: Colors.grey.shade400, width: 2), // Added border
+                    ),
+                    child: _imageFile != null
+                        ? ClipRRect(
+                            borderRadius: BorderRadius.circular(12),
+                            child: Image.file(_imageFile!, fit: BoxFit.cover),
+                          )
+                        : Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Icon(
+                                Icons.add,
+                                size: 48, // Added "+" icon
+                                color: Colors.grey,
+                              ),
+                              const SizedBox(height: 8),
+                              const Text(
+                                'Tambah Foto Barang',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ],
+                          ),
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                _buildFormField(
+                  label: 'Nama Barang',
+                  controller: _namaBarangController,
+                  icon: Icons.inventory,
+                ),
+                const SizedBox(height: 16.0),
+                DropdownButtonFormField<String>(
+                  value: _selectedCategory,
+                  decoration: const InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    labelText: 'Kategori Barang',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.category),
+                  ),
+                  items: _categories
+                      .map((category) => DropdownMenuItem(
+                            value: category,
+                            child: Text(category),
+                          ))
+                      .toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedCategory = value!;
+                    });
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                // Merged Stock input and added "Jumlah Stok" label on top
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Jumlah Stok',
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.remove_circle),
+                            color: Colors.red,
+                            onPressed: _decrementStock,
+                          ),
+                          Container(
+                            width: 80,
+                            child: TextFormField(
+                              controller: _stokController,
+                              keyboardType: TextInputType.number,
+                              decoration: const InputDecoration(
+                                filled: true,
+                                fillColor: Colors.white,
+                                border: OutlineInputBorder(),
+                              ),
+                              textAlign: TextAlign.center,
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Jumlah Stok harus diisi';
+                                }
+                                if (int.tryParse(value) == null) {
+                                  return 'Jumlah Stok harus berupa angka';
+                                }
+                                return null;
+                              },
+                              onChanged: (value) {
+                                setState(() {
+                                  _stock = int.tryParse(value) ?? 0;
+                                });
+                              },
+                            ),
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.add_circle),
+                            color: Colors.green,
+                            onPressed: _incrementStock,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                // Harga input with dollar sign icon
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Harga',
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      TextFormField(
+                        controller: _hargaController,
+                        keyboardType: TextInputType.numberWithOptions(decimal: true),
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(),
+                          prefixIcon: const Icon(Icons.attach_money), // Added dollar sign icon
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) return 'Harga harus diisi';
+                          if (double.tryParse(value.replaceAll(RegExp('[^0-9.]'), '')) == null)
+                            return 'Harga harus berupa angka';
+                          return null;
+                        },
+                        onChanged: (value) {
+                          // No currency formatting logic anymore
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                _buildFormField(
+                  label: 'Deskripsi Barang',
+                  controller: _deskripsiController,
+                  icon: Icons.description,
+                  maxLines: 3,
+                ),
+                const SizedBox(height: 32.0),
+                ElevatedButton(
+                  onPressed: _saveItem,
+                  child: Text('Simpan Barang'),
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 50),
+                    backgroundColor: Colors.green, 
+                    foregroundColor: Colors.white, // Updated property name
+                    textStyle: const TextStyle(fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }// Updated property name
+
+  Widget _buildFormField({
+    required String label,
+    required TextEditingController controller,
+    required IconData icon,
+    int maxLines = 1,
+  }) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.white,
+        labelText: label,
+        border: OutlineInputBorder(),
+        prefixIcon: Icon(icon),
+      ),
+      maxLines: maxLines,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return '$label harus diisi';
+        }
+        return null;
+      },
+    );
+  }
+}
---
+++ lib/screen/welcome_screen.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../screen/home_screen.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _namaTokoController = TextEditingController();
+  final _namaPemilikController = TextEditingController();
+  final _alamatTokoController = TextEditingController();
+
+  @override
+  void dispose() {
+    _namaTokoController.dispose();
+    _namaPemilikController.dispose();
+    _alamatTokoController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _saveDataToSharedPreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('namaToko', _namaTokoController.text.trim());
+    await prefs.setString('namaPemilik', _namaPemilikController.text.trim());
+    await prefs.setString('alamatToko', _alamatTokoController.text.trim());
+  }
+
+  Future<void> _saveDataToFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final profileFile = File('${directory.path}/profile.txt');
+    final profileData = '''
+    Nama Toko: ${_namaTokoController.text.trim()}
+    Nama Pemilik: ${_namaPemilikController.text.trim()}
+    Alamat Toko: ${_alamatTokoController.text.trim()}
+    ''';
+    await profileFile.writeAsString(profileData);
+  }
+
+
+  void _navigateToHome() async {
+    if (_formKey.currentState!.validate()) {
+      await _saveDataToSharedPreferences();
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blue.shade100, Colors.blue.shade300],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RichText(
+                  text: TextSpan(
+                    text: "Kelontong.ID",
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue.shade800,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 40),
+                Form(
+                  key: _formKey,
+                  child: Container(
+                    width: 300, // Reduced width for input boxes
+                    child: Column(
+                      children: [
+                        _buildTextInput(
+                          controller: _namaTokoController,
+                          label: "Nama Toko",
+                          hint: "Masukkan nama toko",
+                        ),
+                        const SizedBox(height: 20),
+                        _buildTextInput(
+                          controller: _namaPemilikController,
+                          label: "Nama Pemilik",
+                          hint: "Masukkan nama pemilik",
+                        ),
+                        const SizedBox(height: 20),
+                        _buildTextInput(
+                          controller: _alamatTokoController,
+                          label: "Alamat Toko",
+                          hint: "Masukkan alamat toko",
+                        ),
+                        const SizedBox(height: 30),
+                        ElevatedButton(
+                          onPressed: _navigateToHome,
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 16, horizontal: 32),
+                            backgroundColor: Colors.blue.shade800,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                          child: const Text(
+                            "Mulai Sekarang",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextInput({
+    required TextEditingController controller,
+    required String label,
+    required String hint,
+  }) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: label,
+        hintText: hint,
+        labelStyle: TextStyle(color: Colors.blue.shade700),
+        filled: true,
+        fillColor: Colors.white,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide.none,
+        ),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return "$label harus diisi";
+        }
+        return null;
+      },
+    );
+  }
+}
